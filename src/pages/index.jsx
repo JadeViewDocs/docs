bcb23f996d6181c0c87c0eccb2257e1c31b0d2b7
tuyangJs <ihanlong@qq.com>
2026-01-15 01:01:23 +0800

    修改标语

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -153,7 +153,7 @@ export default function Homepage() {
               JadeView
             </h2>
             <p className="max-w-xl text-center text-text-400">
-              基于 Rust 的 WebView 窗口库，提供了 C 语言兼容的 API 接口，方便在其他语言（如易语言）中调用和使用。
+             轻量驱动流畅，安全护航开发，JadeView 以简洁接口解锁 WebView 全场景潜力，异步架构赋能无滞交互，极致性能体验，创作自在随心。
             </p>
           </div>
         </section>
